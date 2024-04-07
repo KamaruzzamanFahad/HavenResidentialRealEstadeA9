5d133039389213853e6279b0c468baf0efc59372
KamaruzzamanFahad <mihacker41@gmail.com>
2024-04-08 02:08:11 +0600

    slider added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
 
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+
+
+// import required modules
+import { Navigation } from 'swiper/modules';
 const Home = () => {
+
+
+
     return (
         <div>
-            im home
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                <SwiperSlide>
+                    <div id="slide1" className="carousel-item relative h-[35vw] w-full">
+                        <img src="slid1.jpg" className="w-full" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div id="slide1" className="carousel-item relative h-[35vw] w-full">
+                        <img src="slid2.jpg" className="w-full" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div id="slide1" className="carousel-item relative h-[35vw] w-full">
+                        <img src="slid3.jpg" className="w-full" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div id="slide1" className="carousel-item relative h-[35vw] w-full">
+                        <img src="slid4.jpg" className="w-full" />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+
+            <div>
+                <div>
+                    <input type="text" placeholder='Location' name="" id="" />
+                    
+                </div>
+            </div>
         </div>
     );
 };

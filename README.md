e846dbd49381effc7a7ccedf2145b03fa3734b19
KamaruzzamanFahad <mihacker41@gmail.com>
2024-04-12 01:04:49 +0600

    Readme md file updated

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
-# React + Vite
+Haven Residential
+Live Site URL: https://b9a9kamaruzzaman.netlify.app/
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+*Project Overview*
 
-Currently, two official plugins are available:
+Haven Residential is a residential real estate website designed to streamline the home search process for potential buyers and renters. It provides a user-friendly platform to explore available properties, learn about their features, and connect with you, the real estate agent.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+*Features and Functionality*
+
+*Comprehensive Property Listings: Showcase your property listings with clear descriptions, high-quality photos, and virtual tours (if available) to give potential residents a comprehensive view of each home.
+
+*Search and Filter Options: Empower users to refine their search based on specific criteria such as location, price range, number of bedrooms/bathrooms, property type, and desired amenities.
+
+*Contact Form Integration: Make it easy for users to reach out to you directly through a convenient contact form. Capture their inquiries and connect with them promptly to nurture leads.
+
+*Mobile-Responsive Design: Ensure your website renders flawlessly on all devices, from desktops and laptops to tablets and smartphones. This caters to the growing trend of mobile browsing for real estate searches.
+
+*Custom Login System: Implement a secure login system for authorized users to access specific features or manage property listings (if applicable).
+
+*npm package name which i have used for the challenges task*
+
+*React-leaflet
+*React Hook form
+*Swiper slider
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,24 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../Authprovider';
 
 const Profile = () => {
-    const { user } = useContext(AuthContext);
+    const { user, updateuserinfo } = useContext(AuthContext);
 
-    const { email, displayName, phoneNumber, photoURL } = user;
-    console.log(email)
+    const { email, displayName, photoURL } = user;
+    
+    const updatehandle = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const photo = e.target.photo.value;
+        updateuserinfo(name,photo)
+    }
     return (
         <div className='flex flex-col justify-center items-center'>
             <h1 className='mb-8'>Profilel</h1>
             <img src={photoURL} alt="" />
-            <form className='w-full md:w-[50%]' action="">
-                <p className='mt-8 mb-0'>Your Name</p>
-                <input value={displayName} className='w-full p-3 border-2 mt-2' placeholder="Name" type='text' required/>
+            <form onSubmit={updatehandle} className='w-full md:w-[50%]' action="">
                 <p className='mt-4 mb-0'>Your Email</p>
-                <input value={email} className='w-full p-3 border-2 mt-2' placeholder="Email" type='email' required/>
-                <p className='mt-4 mb-0'>Phone</p>
-                <input value={phoneNumber} className='w-full p-3 border-2 mt-2' placeholder="Phone" type='tel'/>
+                <input readOnly value={email} className='w-full p-3 border-2 mt-2' placeholder="Email" name='email' type='email' required />
+                <p className='mt-8 mb-0'>Your Name</p>
+                <input value={displayName} className='w-full p-3 border-2 mt-2' placeholder="Name" name='name' type='text' required />
                 <p className='mt-4 mb-0'>photoURL</p>
-                <input value={photoURL} className='w-full p-3 border-2 mt-2' placeholder="Photo URL" type='url' required/>
+                <input name='photo' value={photoURL} className='w-full p-3 border-2 mt-2' placeholder="Photo URL" type='url' required />
                 <button className='btn w-full bg-blue-500 text-white mt-5 mb-20'>Update Information</button>
             </form>
         </div>
